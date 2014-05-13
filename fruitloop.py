912e32f36a86092e47968271997ab9085d6627fe
stubbornman <rob@harristn.com>
2014-05-13 12:59:22 -0400

    fruitloops

---
+++ fruitloop.py
@@ -0,0 +1,20 @@
+#####One way to go####
+
+#fruit = 'banana'
+#index = 0
+
+#while index < len(fruit):
+#	try:
+#		letter = fruit[index -1]
+#	except:
+#		break
+#	print letter
+#	index = index - 1
+#######################################
+#######################################
+##Another way#######@
+
+fruit = 'bananas'
+
+for charszz in fruit:
+	print charszz
