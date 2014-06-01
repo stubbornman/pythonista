0e2f08c5c2d47635b16c22bffd922ceb306db941
Rob Harris <rob@harristn.com>
2014-05-31 20:23:58 -0400

    Create romeolist.py

---
+++ romeolist.py
@@ -0,0 +1,18 @@
+fname = raw_input("Enter file name: ")
+fh = open(fname)
+lst = list()
+
+for line in fh:
+	words = line.split()
+	for word in words:
+		if word in lst:
+			continue
+		else:
+			lst.append(word)
+		lst.sort()
+		
+  
+  
+print lst
+
+
